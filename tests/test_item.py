2b2724bc36f40194009bcd11e98cda34d6243b18
ooooose <yuuki.oose1990@gmail.com>
2023-11-17 21:12:04 +0900

    テストの記述

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,6 @@
 from starlette.testclient import TestClient
 from src.main import app
+from fastapi import status
 from tests.databases.databases import test_database
 
 client = TestClient(app)
@@ -7,6 +8,7 @@ client = TestClient(app)
 @test_database
 def test_create_item():
     response = client.post(
-        "/items", json={"name": "foo", "password": "fo"}
+        "/items", json={"name": "foo", "description": "bar"}
     )
-    assert response.status_code == 200
+
+    assert response.status_code == status.HTTP_200_OK
