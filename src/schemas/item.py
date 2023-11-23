aff4bdde07a5b567b509ef3840dc9d81deb22fae
ooooose <yuuki.oose1990@gmail.com>
2023-11-23 10:36:18 +0900

    pytestの警告解消

--- src/schemas/item.py
+++ src/schemas/item.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class ItemBase(BaseModel):
     name: str
@@ -9,9 +9,7 @@ class Item(ItemBase):
     pass
 
 class ItemOrm(ItemBase):
+    model_config = ConfigDict(from_attributes=True)
     id: int
     created_at: datetime
     updated_at: datetime
-
-    class Config:
-        from_attributes=True
