e034fdf3bcc90f8b07bee752f2e320f8fb8b4dd5
ooooose <yuuki.oose1990@gmail.com>
2023-11-25 17:35:18 +0900

    make fmt

--- tests/databases/databases.py
+++ tests/databases/databases.py
@@ -20,9 +20,10 @@ from src.db import Base
 #     return setting_db
 
 
-
 def override_session_factory():
-    TEST_DATABASE_URL = "postgresql://postgres-test:password@postgres-db-test:5432/postgres-test"
+    TEST_DATABASE_URL = (
+        "postgresql://postgres-test:password@postgres-db-test:5432/postgres-test"
+    )
     engine = create_engine(
         url=TEST_DATABASE_URL, echo=False, pool_recycle=10, isolation_level="AUTOCOMMIT"
     )
@@ -37,4 +38,3 @@ def override_session_factory():
         yield session
     finally:
         session.close()
-
